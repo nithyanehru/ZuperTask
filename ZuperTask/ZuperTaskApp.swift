61daf052ab9adea72b02b3d8084052468b09589c
Nithya <nithya.info9@gmail.com>
2026-05-09 17:05:50 +0530

    Initial Commit

---
+++ ZuperTask/ZuperTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZuperTaskApp.swift
+//  ZuperTask
+//
+//  Created by Nithya Nehru on 09/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ZuperTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
